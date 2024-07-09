c671d85de989077ac942c2c114a54cd5f202fef4
Luana Reis <reisluanag@gmail.com>
2024-07-09 19:03:45 -0300

    modal dinâmico

---
+++ script.js
@@ -0,0 +1,26 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+//abrir modal
+cartBtn.addEventListener("click", function () {
+    cartModal.style.display = "flex"
+})
+
+//fechar modal
+cartModal.addEventListener("click", function () {
+    if (event.target === cartModal) {
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click", function () {
+    cartModal.style.display = "none"
+})
